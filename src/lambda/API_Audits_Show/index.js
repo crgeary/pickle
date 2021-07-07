8904616b62645528a8dba668c146d5ff56ca240a
Christopher Geary <hello@crgeary.com>
2021-07-07 19:16:27 +0100

    Add WIP for API_Audits_Show

--- src/lambda/API_Audits_Show/index.js
+++ src/lambda/API_Audits_Show/index.js
@@ -1,4 +1,74 @@
-export const handler = async () => {
+import AWS from 'aws-sdk';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient({
+    apiVersion: `2012-10-08`,
+    region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3({
+    apiVersion: `2006-03-01`,
+});
+
+export const handler = async (event) => {
+    const { pathParameters } = event;
+    const { id } = pathParameters;
+
+    let dynamoResponse, s3response, report;
+
+    try {
+        dynamoResponse = await dynamoDB
+            .get({
+                TableName: process.env.AUDITS_TABLE,
+                Key: {
+                    id,
+                },
+            })
+            .promise();
+    } catch (err) {
+        console.error(err);
+        return {
+            isBase64Encoded: false,
+            statusCode: 500,
+            body: 'Unkown Error',
+        };
+    }
+
+    if (!('Item' in dynamoResponse)) {
+        return {
+            isBase64Encoded: false,
+            statusCode: 404,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                message: `Audit not found for id: ${id}`,
+            }),
+        };
+    }
+
+    const audit = dynamoResponse.Item;
+
+    if (audit.resources && Array.isArray(audit.resources)) {
+        const resource = audit.resources.filter((a) => a.type === 'LHJSON').find(() => true);
+        if (resource) {
+            try {
+                s3response = await s3
+                    .getObject({
+                        Bucket: resource.bucket,
+                        Key: resource.key,
+                    })
+                    .promise();
+                report = JSON.parse(s3response.Body.toString());
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+
+    if (!report) {
+        report = {};
+    }
+
     return {
         isBase64Encoded: false,
         statusCode: 200,
@@ -6,7 +76,11 @@ export const handler = async () => {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            status: 'COMPLETE',
+            pickle: {
+                id,
+                resources: audit?.resources,
+            },
+            ...report,
         }),
     };
 };
