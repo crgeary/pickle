55b43e7a7f2ce69189b942c7ecfe19d155ae7afe
Christopher Geary <hello@crgeary.com>
2021-07-07 18:06:40 +0100

    Add WIP for Stream_Audits_Delete

--- src/lambda/Stream_Audits_Delete/index.js
+++ src/lambda/Stream_Audits_Delete/index.js
@@ -1,3 +1,56 @@
+import AWS from 'aws-sdk';
+
+const s3 = new AWS.S3({
+    apiVersion: `2006-03-01`,
+});
+
+const remove = async (id) => {
+    const listOptions = {
+        Bucket: process.env.AUDITS_BUCKET,
+        Prefix: `${id}/`,
+    };
+
+    console.log(`Deleting objects for audit [${id}].`);
+
+    const objects = await s3.listObjects(listOptions).promise();
+
+    if (0 === objects.Contents.length) {
+        console.log(`No objects found for prefix: [${listOptions.Prefix}].`);
+        return;
+    }
+
+    const deleteOptions = {
+        Bucket: process.env.AUDITS_BUCKET,
+        Delete: {
+            Objects: [],
+        },
+    };
+
+    console.log(`Found ${objects.Contents.length} to delete for prefix: [${listOptions.Prefix}]`);
+
+    objects.Contents.forEach((content) => {
+        deleteOptions.Delete.Objects.push({
+            Key: content.Key,
+        });
+    });
+
+    const deletedObjects = await s3.deleteObjects(deleteOptions).promise();
+
+    console.log(`Deleted ${deletedObjects.Deleted.length} for for prefix: [${listOptions.Prefix}].`);
+
+    if (objects.IsTruncated) {
+        await remove(id);
+    }
+};
+
 export const handler = async (event) => {
-    console.log(event);
+    try {
+        for (const record of event.Records) {
+            if (`REMOVE` === record.eventName) {
+                await remove(record.dynamodb.Keys.id.S);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
 };
